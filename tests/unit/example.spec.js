98349eb255e6260c24a0083337bfb8607476f9c2
Solyanikivan <solyanikivan@mail.ru>
2019-12-29 18:31:07 +0200

    unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,17 @@
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+import store from "@/store";
+import Table from "@/components/Table.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe("Table.vue", () => {
+    it("renders a table", () => {
+        const wrapper = mount(Table, { store });
+        expect(wrapper.contains('table')).to.equal(true);
     });
-    expect(wrapper.text()).to.include(msg);
-  });
+
+    it("renders a form", () => {
+        const wrapper = mount(Table, { store });
+        expect(wrapper.contains('form')).to.equal(true);
+    });
 });
+
